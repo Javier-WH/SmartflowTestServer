6043795a1deea3603d69820fbe8f70b06ede74c5
Jorge Rojas <jorge@andinotechnologies.com>
2024-06-11 09:51:30 -0400

    add letter spacing and nav active color

--- src/modules/MainLayout.tsx
+++ src/modules/MainLayout.tsx
@@ -15,22 +15,28 @@ export default function MainLayout() {
                 <nav className="flex flex-grow bg-[#E6E6E6] rounded-full px-4 justify-around mt-10 shadow-xl">
                     <Link
                         to="/home"
-                        className="py-2 flex-grow text-center"
-                        style={{ textDecoration: location.pathname === '/home' ? 'underline' : 'none' }}
+                        className="py-2 flex-grow text-center font-thin"
+                        style={{
+                            ...(location.pathname === '/home' ? { color: '#6A6A6A', fontWeight: 500 } : {}),
+                        }}
                     >
                         HOME
                     </Link>
                     <Link
                         to="/ordenes"
-                        className="py-2 flex-grow text-center"
-                        style={{ textDecoration: location.pathname === '/ordenes' ? 'underline' : 'none' }}
+                        className="py-2 flex-grow text-center font-thin"
+                        style={{
+                            ...(location.pathname === '/ordenes' ? { color: '#6A6A6A', fontWeight: 500 } : {}),
+                        }}
                     >
                         ORDENES
                     </Link>
                     <Link
                         to="/inventario"
-                        className="py-2 flex-grow text-center"
-                        style={{ textDecoration: location.pathname === '/inventario' ? 'underline' : 'none' }}
+                        className="py-2 flex-grow text-center font-thin"
+                        style={{
+                            ...(location.pathname === '/inventario' ? { color: '#6A6A6A', fontWeight: 500 } : {}),
+                        }}
                     >
                         INVENTARIO
                     </Link>
