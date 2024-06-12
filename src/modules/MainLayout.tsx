49fcbe20e8b732d3d0c02f6f60a7ee596778ddac
Jorge Rojas <jorge@andinotechnologies.com>
2024-06-12 16:57:20 -0400

    add max width and border color to navbar

--- src/modules/MainLayout.tsx
+++ src/modules/MainLayout.tsx
@@ -8,13 +8,16 @@ export default function MainLayout() {
     const location = useLocation();
 
     return (
-        <div className="flex flex-col w-full h-full p-12">
+        <div className="flex flex-col w-full h-full p-12 max-w-screen-2xl mx-auto">
             <header className="flex justify-center items-center gap-12 mb-10">
                 <div className="flex justify-center flex-grow">
                     <img src={Logo} alt="Logo" className="w-32 h-32" />
                 </div>
 
-                <nav className="flex flex-grow-[2] max-w-[1200px] bg-white rounded-lg px-4 justify-around mt-10 shadow-sm">
+                <nav
+                    id="main-menu-nav"
+                    className="flex flex-grow-[3] max-w-[1200px] bg-white rounded-lg px-4 justify-around mt-10 shadow-sm"
+                >
                     <Link
                         to="/home"
                         className="py-2 flex-grow text-center font-thin"
