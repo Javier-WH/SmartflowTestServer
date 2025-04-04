a1a6ae0792523dc0d97b9d6cfa8bb2315ab17c11
Francisco Javier Rodriguez Hernandez <javier@andinotechnologies.com>
2025-04-04 09:00:19 -0400

    refactor members component: remove invitation logic and related state management

--- src/modules/joinOrganization/menbers.tsx
+++ src/modules/joinOrganization/menbers.tsx
@@ -31,11 +31,8 @@ export default function Menbers() {
   const {user, signOut } = useAuth();
   const navigate = useNavigate();
   const { getOrganizationBasicData } = useGetOrganizationData();
-  const {inviteUserToOrganization} = useOrganizations();
   const [organization, setOrganization] = useState<Org | null>(null);
-  const [email, setEmail] = useState<string>('');
-  const [isValidEmail, setIsValidEmail] = useState<boolean>(false);
-  const [inviteLink, setInviteLink] = useState<string | null>(null);
+
 
   useEffect(() => {
     if (!slug) return;
@@ -46,46 +43,12 @@ export default function Menbers() {
       .catch(err => console.log(err));
   }, [slug]);
 
-  useEffect(() => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    setIsValidEmail(emailRegex.test(email));
-  }, [email]);
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target;
-    setInviteLink(null);
-    setEmail(value);
-  }
-  const inviteOnClick = () => {
-    const domain = import.meta.env.VITE_DOMAIN ?? 'localhost:5173';
-    const inviterUserId = user?.id;
-    const organizationId = organization?.id;
 
-    if (!organizationId || !email || !inviterUserId) return;
 
-    inviteUserToOrganization(organizationId, email, inviterUserId)
-    .then(res => {
-      if (res.error) {
-        message.error(res.message);
-        return;
-      }
-      const responseData = res.data as ResponseData[]; 
-      const {id} = responseData[0];
-      const link = `${domain}/join/${id}`;
-      setInviteLink(link)
-    })
-    .catch(err => console.log(err));
-  };
 
-  const handleCopyLink = async () => {
-    if (!inviteLink) return;
-    try {
-      await navigator.clipboard.writeText(inviteLink);
-      message.success('Copy to clipboard');
-    } catch (err) {
-      console.error('Error al copiar:', err);
-    }
-  };
+
+
 
   return <>
     <header className="w-full flex justify-between items-center px-8 bg-white py-4 fixed top-0">
@@ -100,31 +63,7 @@ export default function Menbers() {
       <div className="flex justify-between items-center mb-8">
         <h1 className="text-2xl font-semibold">{organization?.name}</h1>
       </div>
-      <div className="mb-6 max-w-md flex gap-[10px] items-end ">
-        <div>
-          <label className="text-gray-500" htmlFor="input-newMember"> New member email </label>
-          <Input
-            id='input-newMember'
-            placeholder="Write new member email"
-            value={email}
-            onChange={handleInputChange}
-          />
-        </div>
-        <Button disabled={!isValidEmail} color={isValidEmail ? "primary" : "default"} onClick={inviteOnClick}>
-          Invite
-        </Button>
-      </div>
 
-      <div>
-        {inviteLink && (
-          <div className="flex flex-col gap-2 p-6 bg-white border border-blue-gray-200 rounded-lg shadow-sm max-w-[600px]">
-            <p>We have sent an invitation email to {email}.</p>
-            <br />
-            <span className="text-gray-500">Click to copy the invite link:</span>
-            <span style={{ cursor: "pointer" }} onClick={handleCopyLink} className="text-blue-600 hover:text-blue-800 hover:underline">{inviteLink}</span>
-          </div>
-        )}
-      </div>
     </section>
   </>
 }
