17cf5969c2c6d87af677df7af2f2743853521148
Jorge Rojas <jorgeluisrojasb@gmail.com>
2025-04-28 14:53:39 -0400

    improve editor focusing and tree width

--- src/modules/home/pages/Home.tsx
+++ src/modules/home/pages/Home.tsx
@@ -7,11 +7,11 @@ import '../css/home.css';
 export default function Home() {
     return (
         <div className="flex flex-col md:flex-row h-full p-4 gap-2">
-            <nav className="w-full md:w-1/3 bg-gray-100 min-w-[350px]">
+            <nav className="w-full md:w-1/4 bg-gray-100 min-w-[350px] rounded-md">
                 <FolderNavigator />
             </nav>
 
-            <section className="w-full md:w-2/3 overflow-hidden">
+            <section className="w-full md:w-3/4 overflow-hidden">
                 <Outlet />
             </section>
         </div>
