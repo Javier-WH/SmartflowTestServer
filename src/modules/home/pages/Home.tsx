0f248109c3cd5736d2616b6c96e71f4a90d99542
Francisco Javier Rodriguez Hernandez <javier@andinotechnologies.com>
2025-04-29 16:56:44 -0400

    feat: Implement folder and page creation with permission checks and error handling

--- src/modules/home/pages/Home.tsx
+++ src/modules/home/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate, useParams } from 'react-router-dom';
 import FolderNavigator from '@/modules/folderNavigator/folderNavigator';
 import '../css/home.css';
 import SearchInput from '@/modules/search/searchInput';
@@ -9,6 +9,8 @@ import CreateOrUpdateFolderModal from '@/modules/folderNavigator/modal/createOrU
 import { useContext } from 'react';
 import { MainContext, MainContextValues } from '@/modules/mainContext';
 import { Folder } from '@/modules/folderNavigator/types/folder';
+import { message } from 'antd';
+import useFilesManager from '@/modules/folderNavigator/hooks/useFileManager';
 
 export default function Home() {
     const {
@@ -17,16 +19,44 @@ export default function Home() {
         onClose: closeCreateFolderModal,
     } = useDisclosure();
 
-    const { setNewFolderRequest } = useContext(MainContext) as MainContextValues;
+    const { setNewFolderRequest, memberRoll } = useContext(MainContext) as MainContextValues;
+    const { createFile } = useFilesManager();
+    const { organization_id: slug } = useParams();
+    const navigate = useNavigate();
 
     const handleCreateFolder = () => {
+        if (!memberRoll?.write) {
+            message.error('You do not have permission to create a folder');
+            return;
+        }
         const newFolder: Folder = {
-            id: crypto.randomUUID(),
             name: '',
-            container: null,
+            container: '7a89a4e6-b484-4a5b-bf94-5277cb45ae9x',
         };
         setNewFolderRequest(newFolder);
-    }
+    };
+
+
+    const handleCreatePage = () => {
+        if (!memberRoll?.write) {
+            message.error('You do not have permission to create a page');
+            return;
+        }
+        if (!slug) {
+            message.error('Cant find organization');
+            return;
+        }
+        createFile('untitled', null, slug).then(res => {
+            if (res.error) {
+                message.error('Error creating page');
+                return;
+            }
+            const id = res.data;
+            if (id){
+                navigate(`/${slug}/edit/${id}`);
+            }
+        });
+    };
 
     return (
         <div className="flex flex-col md:flex-row h-full p-4 gap-2">
@@ -35,7 +65,7 @@ export default function Home() {
 
                 <div className="bg-gray-100 shadow-gray-100 ring-gray-200 ring-1 shadow-md h-full py-1 rounded-md">
                     <div className="flex justify-end gap-1 px-1">
-                        <Button variant="light" isIconOnly>
+                        <Button variant="light" isIconOnly onPress={handleCreatePage}>
                             <IconFilePlus />
                         </Button>
 
@@ -51,8 +81,6 @@ export default function Home() {
             <section className="w-full md:w-3/4 overflow-hidden">
                 <Outlet />
             </section>
-
-            <CreateOrUpdateFolderModal />
         </div>
     );
 }
