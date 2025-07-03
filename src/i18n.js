fc03931f8a0ea180437158327fe4714dafcda1a3
Francisco Javier Rodriguez Hernandez <javier@andinotechnologies.com>
2025-07-03 08:12:46 -0400

    feat: add i18n support with English and Spanish translations for authentication pages

---
+++ src/i18n.js
@@ -0,0 +1,30 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import en from './locales/en.json';
+import es from './locales/es.json';
+
+i18n
+  .use(LanguageDetector) 
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translation: en,
+      },
+      es: {
+        translation: es,
+      },
+    },
+    // i18n-browser-languagedetector configuracion
+    detection: {
+      order: ['navigator'], 
+      caches: ['localStorage'],
+    },
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
