0306b8df09fc64af4be2330c72b5489574ba4e3b
Francisco Javier Rodriguez Hernandez <javier@andinotechnologies.com>
2025-07-28 17:32:31 -0400

    Cambio de logo

--- src/modules/MainLayout.tsx
+++ src/modules/MainLayout.tsx
@@ -8,6 +8,7 @@ import UserMenu from '@/components/ui/UserMenu';
 import { useContext } from 'react';
 import { MainContextValues, MainContext } from './mainContext';
 import { useNavigate } from 'react-router-dom';
+import logo from "../assets/png/smartfloLogoB.png"
 
 
 
@@ -18,7 +19,8 @@ function Header() {
     return (
         <header className="flex justify-end md:justify-between items-center px-8 w-full h-[70px] top-0 bg-gray-100 shadow-md">
             <h1 className="max-md:hidden md:block font-[300] text-[40px] tracking-[0.3rem] cursor-pointer" onClick={() => {setParentFolders(''); navigate('/organizations')}}>
-                <span className="text-primary">S</span>MAR<span className="text-primary">T</span>FLO
+               { /*<span className="text-primary">S</span>MAR<span className="text-primary">T</span>FLO*/}
+               <img src={logo} alt="logo" style={{width: "400px", height: "70px"}}/>
             </h1>
             <span className="text-primary text-[18px] text-left min-w-[200px] overflow-x-auto whitespace-nowrap scrollbar-thumb-primary scrollbar-track-transparent scrollbar-thin">
                 <span className="font-bold text-[20px]">
