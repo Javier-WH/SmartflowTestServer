fa358f663f1bb44a0f2af244f027e0740561709c
Francisco Javier Rodriguez Hernandez <javier@andinotechnologies.com>
2025-08-28 18:27:49 -0400

    Reparado bug de titulo, eliminado el cache de consulta

--- src/modules/folderNavigator/hooks/useFileContent.ts
+++ src/modules/folderNavigator/hooks/useFileContent.ts
@@ -8,8 +8,9 @@ export default function useFileContent({ fileId }: { fileId: string }) {
         error,
         count,
     } = useQuery(supabase.from('filesquill').select('content, name, updated_at').eq('id', fileId).single(), {
-        revalidateOnFocus: false,
-        revalidateOnReconnect: false,
+        revalidateOnFocus: true,
+        revalidateOnReconnect: true,
+        revalidateIfStale: true
     });
 
     const { trigger: update, isMutating } = useUpdateMutation(
