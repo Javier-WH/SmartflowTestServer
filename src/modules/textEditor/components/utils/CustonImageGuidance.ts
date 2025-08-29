175e457a7ee301fbb5ab7a545b036a13f9133ea3
Francisco Javier Rodriguez Hernandez <javier@andinotechnologies.com>
2025-08-29 15:04:53 -0400

    added image upload functionality with placeholder during upload

--- src/modules/textEditor/components/utils/CustonImageGuidance.ts
+++ src/modules/textEditor/components/utils/CustonImageGuidance.ts
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import Quill from "quill";
+import { uploadImageToStorage } from '../../imgStorage/imgStorage';
+import PLACEHOLDER_SVG from "./LoadingImageSpinner.svg"
 
 const Image = Quill.import('formats/image') as any;
 export default class CustomImageGuidance extends Image {
@@ -7,6 +9,23 @@ export default class CustomImageGuidance extends Image {
     // When creating the image, we check if 'value' is an object that includes src, width, and height
     static create(value: any) {
         const node = super.create(value);
+        const handleUpload = async (src: string) => {
+            try {
+                node.classList.add('uploadingImage');
+                const base64Data = src.split(';base64,')[1];
+                node.setAttribute('src', PLACEHOLDER_SVG);
+                const url = await uploadImageToStorage(base64Data, "testImage");
+                node.setAttribute('src', url);
+            } catch (error) {
+                console.error('Error uploading image:', error);
+                node.setAttribute('src', src); // Mantiene base64 si hay error
+            } finally {
+                node.classList.remove('uploadingImage');
+            }
+        };
+        if (typeof value === 'string' && value.startsWith('data:image/')) {
+            handleUpload(value);
+        } 
 
         // Only if the image does not already have a defined width, assign the default size
         if (!node.getAttribute("width")) {
