192d4947771775ff3c8dd5e8586b712279651f37
Francisco Javier Rodriguez Hernandez <javier@andinotechnologies.com>
2025-09-09 10:52:02 -0400

    feat: implement create_file_quill function for file creation with organization support

--- src/SQL/versionPatcvh.sql
+++ src/SQL/versionPatcvh.sql
@@ -26,3 +26,36 @@ EXCEPTION
         RETURN FALSE;
 END;
 $$;
+
+
+
+CREATE OR REPLACE FUNCTION create_file_quill(
+  p_name varchar(100),
+  p_container uuid DEFAULT NULL,
+  p_slug TEXT DEFAULT NULL
+) 
+RETURNS uuid AS $$
+DECLARE
+  new_id uuid;
+  p_organization_id uuid;
+BEGIN
+  -- obtener el organization_Id usando el slug
+
+    SELECT o.id INTO p_organization_id FROM public.organizations o WHERE o.slug =  p_slug;
+
+  -- Insertar el registro manejando el caso especial para container
+  INSERT INTO public.filesquill(name, container, organization_id, content)
+  VALUES (
+    p_name,
+    CASE 
+      WHEN p_container IS NULL THEN NULL
+      ELSE p_container
+    END,
+    p_organization_id,
+    '<p><br></p><guided-checklist class="guided-checklist-block" title="" items="[{&quot;id&quot;:&quot;fd2390ff-4643-4dd1-9622-9f5061186ea7&quot;,&quot;index&quot;:0,&quot;text&quot;:&quot;&quot;,&quot;guidande&quot;:&quot;&quot;}]" contenteditable="false" readonly="false"></guided-checklist><p><br></p><p><br></p>'
+  )
+  RETURNING id INTO new_id;
+  
+  RETURN new_id;
+END;
+$$ LANGUAGE plpgsql;
