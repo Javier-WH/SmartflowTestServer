468d718b2422b9549f4ed4cebfc662a2e9c37ce8
fj_rh@hotmail.com <javier@andinotechnologies.com>
2025-10-29 11:04:19 -0400

    Refactor UserMenu component to replace "organization" terminology with "working group" and update related navigation logic

--- src/components/ui/UserMenu.tsx
+++ src/components/ui/UserMenu.tsx
@@ -4,21 +4,17 @@ import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { MainContext, type MainContextValues } from '@/modules/mainContext';
 import { useTranslation } from 'react-i18next';
 import { useContext } from 'react';
-//import MultipleInviteUserModal from '../MultipleInviteUserModal';
 
 export default function UserMenu() {
     const { t } = useTranslation();
     const location = useLocation();
     const navigate = useNavigate();
-    const { organization_id } = useParams();
+    const { working_group_id } = useParams();
     const { setParentFolders } = useContext(MainContext) as MainContextValues;
     const isMembersPage = location.pathname.endsWith('/members');
     const { user, signOut } = useAuth();
-    //const [openInviteModal, setOpenInviteModal] =  useState(false);
 
-    //const isHomePage = location.pathname === '/home';
-    //const isOrganizationsPage = location.pathname.startsWith('/organizations');
-
+    console.log(useParams());
     const renderMenu = () => {
         const menuItems = [];
 
@@ -26,15 +22,15 @@ export default function UserMenu() {
           
         }*/
 
-        if (organization_id) {
+        if (working_group_id) {
             menuItems.push(
                 <DropdownItem key="organizations" onPress={() => { navigate('/organizations'); setParentFolders(''); }}>
-                    {t('organizations_label')}
+                    {t('working_groups_label')}
                 </DropdownItem>,
                 <DropdownItem
                     key="members"
                     onPress={() => {
-                        const target = isMembersPage ? `/${organization_id}/home` : `/${organization_id}/members`;
+                        const target = isMembersPage ? `/${working_group_id}/home` : `/${working_group_id}/members`;
                         navigate(target);
                         setParentFolders('');
                     }}
@@ -73,25 +69,25 @@ export default function UserMenu() {
     };
 
 
-    return (
-        <div className="flex items-center gap-4">
-            {/*<MultipleInviteUserModal isOpen={openInviteModal} onClose={() => setOpenInviteModal(false)} userId={user?.id || ''} />*/}
-            <Dropdown placement="bottom-start">
-                <DropdownTrigger>
-                    <User
-                        as="button"
-                        className="users__avatar flex items-center transition-transform hover:bg-primary/10 py-1 px-2 mr-"
-                        name={`${user?.user_metadata?.name} ${user?.user_metadata?.lastname}`}
-                        avatarProps={{
-                            size: 'sm',
-                        }}
-                        classNames={{
-                            name: 'capitalize text-md ml-[10px]',
-                        }}
-                    />
-                </DropdownTrigger>
-                {renderMenu()}
-            </Dropdown>
-        </div>
-    );
-}
+        return (
+            <div className="flex items-center gap-4">
+         
+                <Dropdown placement="bottom-start">
+                    <DropdownTrigger>
+                        <User
+                            as="button"
+                            className="users__avatar flex items-center transition-transform hover:bg-primary/10 py-1 px-2 mr-"
+                            name={`${user?.user_metadata?.name} ${user?.user_metadata?.lastname}`}
+                            avatarProps={{
+                                size: 'sm',
+                            }}
+                            classNames={{
+                                name: 'capitalize text-md ml-[10px]',
+                            }}
+                        />
+                    </DropdownTrigger>
+                    {renderMenu()}
+                </Dropdown>
+            </div>
+        );
+    }
